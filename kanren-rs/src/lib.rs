fb28809576efa7e46824496f28553a4bfa29840e
IsaacDynamo <isaacdynamo@gmail.com>
2023-03-18 14:51:26 +0100

    Reorder code

--- kanren-rs/src/lib.rs
+++ kanren-rs/src/lib.rs
@@ -3,6 +3,13 @@ mod display;
 
 use std::{cell::RefCell, collections::HashMap, fmt::Debug, rc::Rc};
 
+// TODO:
+// - neq()
+// - impl Goal
+// - Prefer non-yield goals in eval of Both
+// - Add bool and str
+// - Use term arguments in custom goals
+
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub struct Var {
     id: u32,
@@ -14,17 +21,6 @@ pub enum Term {
     Var(u32),
     Cons(Rc<Term>, Rc<Term>),
     Null,
-}
-
-type Mapping = HashMap<u32, Term>;
-
-#[derive(Clone)]
-pub enum Goal {
-    Eq(Term, Term),
-    Both(Rc<Goal>, Rc<Goal>),
-    Either(Rc<Goal>, Rc<Goal>),
-    Fresh(Rc<dyn Fn(&mut State) -> Goal>, RefCell<Option<Box<Goal>>>),
-    Yield(Rc<dyn Fn() -> Goal>, Rc<RefCell<Option<Goal>>>),
 }
 
 impl From<i32> for Term {
@@ -39,6 +35,14 @@ impl From<Var> for Term {
     }
 }
 
+pub fn cons(a: impl Into<Term>, b: impl Into<Term>) -> Term {
+    Term::Cons(Rc::new(a.into()), Rc::new(b.into()))
+}
+
+pub const NULL: Term = Term::Null;
+
+type Mapping = HashMap<u32, Term>;
+
 fn resolve<'a>(term: &'a Term, map: &'a Mapping) -> &'a Term {
     use Term::*;
     match term {
@@ -50,6 +54,15 @@ fn resolve<'a>(term: &'a Term, map: &'a Mapping) -> &'a Term {
             }
         }
         _ => term,
+    }
+}
+
+fn deep_resolve(term: &Term, map: &Mapping) -> Term {
+    let term = resolve(term, map);
+
+    match term {
+        Term::Cons(a, b) => cons(deep_resolve(a, map), deep_resolve(b, map)),
+        _ => term.clone(),
     }
 }
 
@@ -79,14 +92,37 @@ fn unify(a: &Term, b: &Term, map: &Mapping) -> Option<Mapping> {
     }
 }
 
-pub const NULL: Term = Term::Null;
+#[derive(Default, Debug, Clone)]
+pub struct State {
+    map: Mapping,
+    id: u32,
+}
+
+impl State {
+    fn resolve(&self, v: Var) -> Term {
+        let term = Term::Var(v.id);
+        deep_resolve(&term, &self.map)
+    }
+
+    fn var(&mut self) -> Var {
+        let id = self.id;
+        assert_ne!(id, u32::MAX, "Overflow");
+        self.id += 1;
+        Var { id }
+    }
+}
+
+#[derive(Clone)]
+pub enum Goal {
+    Eq(Term, Term),
+    Both(Rc<Goal>, Rc<Goal>),
+    Either(Rc<Goal>, Rc<Goal>),
+    Fresh(Rc<dyn Fn(&mut State) -> Goal>, RefCell<Option<Box<Goal>>>),
+    Yield(Rc<dyn Fn() -> Goal>, Rc<RefCell<Option<Goal>>>),
+}
 
 pub fn eq(a: impl Into<Term>, b: impl Into<Term>) -> Goal {
     Goal::Eq(a.into(), b.into())
-}
-
-pub fn cons(a: impl Into<Term>, b: impl Into<Term>) -> Term {
-    Term::Cons(Rc::new(a.into()), Rc::new(b.into()))
 }
 
 pub fn both(a: Goal, b: Goal) -> Goal {
@@ -95,6 +131,76 @@ pub fn both(a: Goal, b: Goal) -> Goal {
 
 pub fn either(a: Goal, b: Goal) -> Goal {
     Goal::Either(Rc::new(a), Rc::new(b))
+}
+
+pub fn fresh<T>(f: impl Binding<T> + Copy + 'static) -> Goal {
+    Goal::Fresh(Rc::new(move |state| f.bind(state).1), RefCell::new(None))
+}
+
+pub fn jield(f: impl Fn() -> Goal + 'static) -> Goal {
+    Goal::Yield(Rc::new(f), Rc::new(RefCell::new(None)))
+}
+
+pub fn all(v: impl IntoIterator<Item = Goal>) -> Goal {
+    fn inner(mut iter: impl Iterator<Item = Goal>) -> Option<Goal> {
+        let a = iter.next()?;
+        match inner(iter) {
+            Some(b) => Some(both(a, b)),
+            None => Some(a),
+        }
+    }
+
+    inner(v.into_iter()).unwrap()
+}
+
+pub fn any(v: impl IntoIterator<Item = Goal>) -> Goal {
+    fn inner(mut iter: impl Iterator<Item = Goal>) -> Option<Goal> {
+        let a = iter.next()?;
+        match inner(iter) {
+            Some(b) => Some(either(a, b)),
+            None => Some(a),
+        }
+    }
+
+    inner(v.into_iter()).unwrap()
+}
+
+pub fn cond<T, R>(table: T) -> Goal
+where
+    T: IntoIterator<Item = R>,
+    R: IntoIterator<Item = Goal>,
+{
+    any(table.into_iter().map(|goals| all(goals)))
+}
+
+#[derive(Default)]
+pub struct Stream {
+    mature: Vec<State>,
+    immature: Option<Box<dyn FnOnce() -> Stream>>,
+}
+
+// impl Debug for Stream {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         f.debug_struct("Stream")
+//             .field("mature", &self.mature)
+//             .field("immature", &self.immature.as_ref().map(|_| ()))
+//             .finish()
+//     }
+// }
+
+impl Stream {
+    pub fn is_empty(&self) -> bool {
+        self.mature.is_empty() && self.immature.is_none()
+    }
+
+    pub fn pull(&mut self) {
+        let immature = self.immature.take();
+        if let Some(cont) = immature {
+            let s = cont();
+            self.mature.extend(s.mature);
+            self.immature = s.immature;
+        }
+    }
 }
 
 fn append(mut a: Stream, b: Stream) -> Stream {
@@ -131,93 +237,6 @@ fn mappend(goal: &Goal, stream: Stream) -> Stream {
         }
     } else {
         unreachable!()
-    }
-}
-
-#[derive(Default, Debug, Clone)]
-pub struct State {
-    map: Mapping,
-    id: u32,
-}
-
-impl State {
-    fn resolve(&self, v: Var) -> Term {
-        let term = Term::Var(v.id);
-        deep_resolve(&term, &self.map)
-    }
-
-    fn var(&mut self) -> Var {
-        let id = self.id;
-        assert_ne!(id, u32::MAX, "Overflow");
-        self.id += 1;
-        Var { id }
-    }
-}
-
-#[derive(Default)]
-pub struct Stream {
-    mature: Vec<State>,
-    immature: Option<Box<dyn FnOnce() -> Stream>>,
-}
-
-impl Debug for Stream {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Stream")
-            .field("mature", &self.mature)
-            .field("immature", &self.immature.as_ref().map(|_| ()))
-            .finish()
-    }
-}
-
-impl Stream {
-    pub fn is_empty(&self) -> bool {
-        self.mature.is_empty() && self.immature.is_none()
-    }
-
-    pub fn pull(&mut self) {
-        let immature = self.immature.take();
-        if let Some(cont) = immature {
-            let s = cont();
-            self.mature.extend(s.mature);
-            self.immature = s.immature;
-        }
-    }
-}
-
-impl IntoIterator for Stream {
-    type Item = State;
-
-    type IntoIter = StreamIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        Self::IntoIter {
-            mature: self.mature.into_iter(),
-            immature: self.immature,
-        }
-    }
-}
-
-pub struct StreamIter {
-    mature: std::vec::IntoIter<State>,
-    immature: Option<Box<dyn FnOnce() -> Stream>>,
-}
-
-impl Iterator for StreamIter {
-    type Item = State;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let r = self.mature.next();
-        if r.is_some() {
-            r
-        } else {
-            let immature = self.immature.take();
-            if let Some(cont) = immature {
-                *self = cont().into_iter();
-                self.next()
-            } else {
-                None
-            }
-        }
     }
 }
 
@@ -265,44 +284,40 @@ impl Goal {
     }
 }
 
-pub fn all(v: impl IntoIterator<Item = Goal>) -> Goal {
-    fn inner(mut iter: impl Iterator<Item = Goal>) -> Option<Goal> {
-        let a = iter.next()?;
-        match inner(iter) {
-            Some(b) => Some(both(a, b)),
-            None => Some(a),
+impl IntoIterator for Stream {
+    type Item = State;
+
+    type IntoIter = StreamIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            mature: self.mature.into_iter(),
+            immature: self.immature,
         }
     }
-
-    inner(v.into_iter()).unwrap()
 }
 
-pub fn any(v: impl IntoIterator<Item = Goal>) -> Goal {
-    fn inner(mut iter: impl Iterator<Item = Goal>) -> Option<Goal> {
-        let a = iter.next()?;
-        match inner(iter) {
-            Some(b) => Some(either(a, b)),
-            None => Some(a),
+pub struct StreamIter {
+    mature: std::vec::IntoIter<State>,
+    immature: Option<Box<dyn FnOnce() -> Stream>>,
+}
+
+impl Iterator for StreamIter {
+    type Item = State;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let r = self.mature.next();
+        if r.is_some() {
+            r
+        } else {
+            let immature = self.immature.take();
+            if let Some(cont) = immature {
+                *self = cont().into_iter();
+                self.next()
+            } else {
+                None
+            }
         }
-    }
-
-    inner(v.into_iter()).unwrap()
-}
-
-pub fn cond<T, R>(table: T) -> Goal
-where
-    T: IntoIterator<Item = R>,
-    R: IntoIterator<Item = Goal>,
-{
-    any(table.into_iter().map(|goals| all(goals)))
-}
-
-fn deep_resolve(term: &Term, map: &Mapping) -> Term {
-    let term = resolve(term, map);
-
-    match term {
-        Term::Cons(a, b) => cons(deep_resolve(a, map), deep_resolve(b, map)),
-        _ => term.clone(),
     }
 }
 
@@ -368,12 +383,4 @@ pub fn runx<T>(n: usize, f: impl Binding<T>) -> Vec<Vec<Term>> {
         .map(|s| q.vars.iter().map(|v| s.resolve(*v)).collect::<Vec<Term>>())
         .take(n)
         .collect()
-}
-
-pub fn fresh<T>(f: impl Binding<T> + Copy + 'static) -> Goal {
-    Goal::Fresh(Rc::new(move |state| f.bind(state).1), RefCell::new(None))
-}
-
-pub fn jield(f: impl Fn() -> Goal + 'static) -> Goal {
-    Goal::Yield(Rc::new(f), Rc::new(RefCell::new(None)))
 }
